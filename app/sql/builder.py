ddaf133fcf7285e5c7184619843b70ab94931cfb
ngevorkyan <gevorkyan.nino@gmail.com>
2026-05-10 20:07:21 +0400

    added new metrics

--- app/sql/builder.py
+++ app/sql/builder.py
@@ -1,7 +1,12 @@
 def build_sql(metric_data: dict, segment: str = "all") -> str:
     sql = metric_data["base_sql"]
 
-    segment_filter = metric_data.get("segments", {}).get(segment)
+    segment_data = metric_data.get("segments", {}).get(segment)
+
+    if isinstance(segment_data, dict):
+        segment_filter = segment_data.get("filter")
+    else:
+        segment_filter = segment_data
 
     if segment_filter:
         sql += f" WHERE {segment_filter}"
